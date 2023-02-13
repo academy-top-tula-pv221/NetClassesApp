0168f2efcff42c36ced3b0ac001becff989f3d24
maxefimov <efimov.academy.top@gmail.com>
2023-02-13 21:23:53 +0300

    Добавьте файлы проекта.

---
+++ NetClassesApp/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace NetClassesApp
+{
+    struct PersonStruct
+    {
+        public string name;
+        public int age;
+        public Address address;
+        public PersonStruct(string name, int age, Address address) 
+        { 
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+
+        public void Write()
+        {
+            Console.WriteLine($"{name}  {age}");
+        }
+    }
+
+    class Address
+    {
+        public string city;
+        public string street;
+
+        public void Write()
+        {
+            Console.WriteLine($"{city}  {street}");
+        }
+    }
+
+    class Person
+    {
+        public string name = "Anonim";
+        public int age = 2;
+        public void Write()
+        {
+            Console.WriteLine($"{name}  {age}");
+        }
+
+        public void Deconstruct(out string name, out int age)
+        {
+            name = this.name;
+            age = this.age;
+        }
+    }
+}
---
+++ NetClassesApp/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace NetClassesApp
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Address address = new Address()
+            {
+                city = "Moscow",
+                street = "Tverskaya"
+            };
+
+            address.Write();
+
+            AddressWork(ref address);
+
+            address.Write();
+
+            //Address address2 = address;
+            //address2.city = "Tula";
+            //address.Write();
+
+            //PersonStruct person1 = new() { name = "Bob", age = 23, address = address };
+            //person1.Write();
+            //person1.address.Write();
+
+            //PersonStruct person2 = person1;
+            //person2.name = "Joe";
+            //person2.address.city = "Tula";
+
+            //person1.Write();
+            //person1.address.Write();
+        }
+
+        static void AddressWork(ref Address address)
+        {
+            address.city = "Orel";
+            address = new Address() { city = "Ufa", street = "Lenina" };
+            address.Write();
+        }
+    }
+}
